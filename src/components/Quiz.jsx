b5540617c96c5f082dded914f44a995c223379be
evelinsari <falatovics.evelin@gmail.com>
2024-01-09 12:43:31 +0100

    outputting questions, shuffling answers

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+import QUESTIONS from '../questions'
+
+
+export function Quiz() {
+    const [userAnswers, setUserAnswers] = useState([])
+
+    const activeQuestionIndex = userAnswers.length
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers]
+    shuffledAnswers.sort(() => Math.random() -0.5)
+
+    function handleSelectAnswer (selectedAnswer) {
+        setUserAnswers((prevUserAnswers)=> {
+            return [...prevUserAnswers, selectedAnswer]
+        })
+    }
+
+    return (
+        <div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">
+                {shuffledAnswers.map(answer => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                    </li>
+                ) )}
+            </ul>
+        </div> 
+        </div>
+    )
+}
